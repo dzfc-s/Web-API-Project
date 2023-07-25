5ffa5cc5b6915d91ed31e410321b221cbc19ce09
“Ime <selma.dzafic@corussoft.de>
2023-07-25 16:01:13 +0200

    create GetAllUsers and GetUsersById HTTP methods

---
+++ Web API Project/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web_API_Project.Models;
+
+namespace Web_API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static List<User> users = new List<User>
+        {
+            new User { Id = 1, FirstName = "Selma", LastName = "Dzafic", Role = "Student"},
+            new User { Id = 2, FirstName = "Nudzejm", LastName = "Delanovic", Role = "Student" }
+        };
+
+        [HttpGet("GetAllUsers")]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("GetUserById")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            var user = users.Find(x => x.Id == id);
+            if (user is null)
+                return NotFound($"User with id {id} does not exist! ");
+            return Ok(user);
+        }
+    }
+}
---
+++ Web API Project/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Web_API_Project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
